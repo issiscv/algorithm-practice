782fbdba867e9e107def1f0972d0b6a845015b86
issiscv <issiscv46@gmail.com>
2022-03-10 15:49:16 +0900

    BOJ DP 2579, 1699

---
+++ src/baekjoon/DP/DP1699.java
@@ -0,0 +1,23 @@
+package baekjoon.DP;
+
+import java.util.Scanner;
+
+public class DP1699 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int N = scan.nextInt();
+
+        int[] dp = new int[N+1];
+        dp[1] = 1;
+
+        for (int i = 2; i <= N; i++) {
+            dp[i] = i;
+            for (int j = 1; j*j <= i; j++) {
+                dp[i] = Math.min(dp[i], dp[i-j*j]+1);
+            }
+        }
+
+        System.out.println(dp[N]);
+    }
+}
--- src/baekjoon/DP/DP2579.java
+++ src/baekjoon/DP/DP2579.java
@@ -4,37 +4,28 @@ import java.util.Scanner;
 
 public class DP2579 {
 
-    static int[] arr;
-    static Integer[] dp;
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         int N = scanner.nextInt();
-        arr = new int[N+1];
-        dp = new Integer[N+1];
+
+        int[] arr = new int[N+1];
+        int[] dp = new int[N+1];
 
         for (int i = 1; i <= N; i++) {
             arr[i] = scanner.nextInt();
         }
 
-        dp[0] = 0;
         dp[1] = arr[1];
 
         if (N >= 2) {
             dp[2] = arr[1] + arr[2];
         }
 
-        int recursive = recursive(N);
-        System.out.println(recursive);
-
-    }
-
-    private static int recursive(int i) {
-
-        if (dp[i] == null) {
-            dp[i] = Math.max(recursive(i-2), recursive(i-3) + arr[i-1]) + arr[i];
+        for (int i = 3; i <= N; i++) {
+            dp[i] = Math.max(dp[i-2], dp[i-3] + arr[i-1]) + arr[i];
         }
-        return dp[i];
+
+        System.out.println(dp[N]);
     }
 }
