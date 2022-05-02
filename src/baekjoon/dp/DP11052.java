10077a11c8a2714be4aa30da583ec646a001033d
issiscv <issiscv46@gmail.com>
2022-05-02 23:59:29 +0900

    BOJ DP recap 2225, 2011, 11052

--- src/baekjoon/dp/DP11052.java
+++ src/baekjoon/dp/DP11052.java
@@ -8,20 +8,22 @@ public class DP11052 {
         Scanner scanner = new Scanner(System.in);
 
         int N = scanner.nextInt();
-        int[] arr = new int[N+1];
-        int[] dp = new int[N+1];
 
+        int[] arr = new int[N+1];
         for (int i = 1; i <= N; i++) {
             arr[i] = scanner.nextInt();
         }
 
+        int[] dp = new int[N+1];
         dp[1] = arr[1];
-
         for (int i = 2; i <= N; i++) {
             for (int j = 1; j <= i; j++) {
                 dp[i] = Math.max(dp[i], dp[i-j] + arr[j]);
             }
+
         }
         System.out.println(dp[N]);
+
     }
+
 }
--- src/baekjoon/dp/DP2225.java
+++ src/baekjoon/dp/DP2225.java
@@ -10,23 +10,24 @@ public class DP2225 {
         int N = scanner.nextInt();
         int K = scanner.nextInt();
 
-        int[][] dp = new int[N+1][K+1];
+        int[][] arr = new int[K+1][N+1];
 
         for (int i = 1; i <= N; i++) {
-            dp[i][0] = 0;
+            arr[1][i] = 1;
         }
 
         for (int i = 1; i <= K; i++) {
-            dp[0][i] = 1;
+            arr[i][0] = 1;
         }
 
-        for (int i = 1; i <= N; i++) {
-            for (int j = 1; j <= K; j++) {
-                dp[i][j] = (dp[i-1][j] + dp[i][j-1]) % 1000000000;
+        for (int i = 2; i <= K; i++) {
+            for (int j = 1; j <= N; j++) {
+                arr[i][j] = (arr[i-1][j] + arr[i][j-1]) % 1000000000;
             }
         }
 
-        System.out.println(dp[N][K]);
-    }
+        System.out.println(arr[K][N]);
 
+    }
+    
 }
