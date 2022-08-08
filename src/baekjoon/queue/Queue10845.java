e09c6b820cd4d45ba6a33f1ddcaefba92024b530
issiscv <issiscv46@gmail.com>
2022-08-08 19:59:44 +0900

    queue 복습

--- src/baekjoon/queue/Queue10845.java
+++ src/baekjoon/queue/Queue10845.java
@@ -1,25 +1,29 @@
 package baekjoon.queue;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Queue10845 {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int N = scanner.nextInt();
-
-        LinkedList<Integer> queue = new LinkedList<>();
+        int N = Integer.parseInt(br.readLine());
 
         StringBuilder sb = new StringBuilder();
 
+        LinkedList<Integer> queue = new LinkedList<>();
         for (int i = 0; i < N; i++) {
-            String str = scanner.next();
+            String str = br.readLine();
+            StringTokenizer st = new StringTokenizer(str);
 
-            if (str.equals("push")) {
-                queue.offer(scanner.nextInt());
+            if (st.nextToken().equals("push")) {
+                queue.offer(Integer.parseInt(st.nextToken()));
             } else if (str.equals("pop")) {
                 if (queue.isEmpty()) sb.append("-1\n");
                 else sb.append(queue.poll() + "\n");
@@ -36,6 +40,7 @@ public class Queue10845 {
                 else sb.append(queue.get(queue.size()-1)+"\n");
             }
         }
-        System.out.println(sb);
+
+        System.out.println(sb.toString());
     }
 }
